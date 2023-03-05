586bd21d8c299ccd7a9804ea56904d95be01ef70
Roosevelt Seymour <RSeymourJR04@outlook.com>
2023-03-05 18:51:55 +0000

    Completed Combine array Challenge

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,18 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int array1 = arr1.length;
+        int array2 = arr2.length;
+        int both = array1 + array2;
 
-        return null;
+        int[] a1a2 = new int[both];
+        for( int i = 0; i < array1; i++){
+            a1a2[i] = arr1[i];
+        }
+        for ( int i = 0; i < array2; i++){
+            a1a2[array1+i] = arr2[i];
+        }
+
+        return a1a2;
     }
 }
